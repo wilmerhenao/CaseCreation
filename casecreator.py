961aee8c2d1a0e9a6803491e53111c20507e1f64
Wilmer E. Henao <wilmer@umich.edu>
2016-06-13 11:58:57 -0500

    implemented geoloc class

--- casecreator.py
+++ casecreator.py
@@ -18,34 +18,64 @@ import scipy as sp
 class caseinfo:
     isoX = 0.0
     isoY = 0.0
-    isoZ = 0.0
-    def __init__(self, x = 0.0, y = 0.0, z = 0.0):
-        isoX = x
-        isoY = y
-        isoZ = z
+    R = 1.0
+    def __init__(self, x = 0.0, y = 0.0, radio):
+        self.isoX = x
+        self.isoY = y
+        self.R = radio
 ## Initialize an instance. This is not necessary but it is good practice.
-thiscase = caseinfo(0.0, 0.0, 0.0)
+thiscase = caseinfo(0.0, 0.0, 1.0)
 
-## Class that uses a data triplet and implements some geographical operations
+## Class that uses a data pair and implements some geographical operations. Depth of the voxel given beam.
 class geoloc:
-    def __init__(self, x = 0.0, y = 0.0, z = 0.0):
+    def __init__(self, x = 0.0, y = 0.0):
         self.x = x
         self.y = y
-        self.z = z
+        self.depth = None
     ## This function calculates the distance from my geographical location to the center of a beamlet
     def distToBeamC(self, xBeamC, yBeamC , zBeamC ):
-        d = np.sqrt(np.sum((self.x - xBeamC )^2 + (self.y - yBeamC )^2 + (self.z - zBeamC )^2))
+        d = np.sqrt(np.sum((self.x - xBeamC )^2 + (self.y - yBeamC )^2))
         return(d)
     ## Function to calculate distance from this point to isocenter
-    def distToIsoC(self, xisoC, yisoC , zisoC ):
-        d = np.sqrt(np.sum((self.x - xisoC )^2 + (self.y - yisoC )^2 + (self.z - zisoC )^2))
+    def distToIsoC(self):
+        d = np.sqrt(np.sum((self.x - thiscase.isoX)^2 + (self.y - thiscase.isoY)^2))
         return(d)
 
+    ## This function finds whether a point lies INSIDE the line SEGMENT between the beamlet and the voxel or not.
+    def isinterior(self, xinterp, xBeamC):
+        ininterior = False
+        if (np.min(xBeamC, self.x) <= xinterp and xinterp <= np.max(xBeamC, self.x)):
+            ininterior = True
+        return(ininterior)
+
+    ## Find the depth of this voxel inside the body
     def depthBeamC(self, xBeamC, yBeamC, zBeamC, dBeamlettoIso):
-        ## Use the cosine law to calculate the angle formed by the line from isocenter to this point and the line from
-        # beamlet center to this point.
-        c = dBeamlettoIso
-        a = distToIsoC()
+        ## To understand the methodology look at http://mathworld.wolfram.com/Circle-LineIntersection.html
+        x2 = xBeamC
+        y2 = yBeamC
+        x1 = self.x
+        y1 = self.y
+        dx = x2 - x1
+        dy = y2 - y1
+        dr = np.sqrt(dx^2 + dy^2)
+        D = x1 * y2 - x2 * y1
+        ## There are two point of intersection
+        xinterp = (D * dy + np.sign(dy) * dx * np.sqrt(caseinfo.R^2 * dr^2 - D^2))/(dr^2)
+        xinterm = (D * dy - np.sign(dy) * dx * np.sqrt(caseinfo.R^2 * dr^2 - D^2))/(dr^2)
+        yinterp = (-D * dx + np.abs(dy) * np.sqrt(caseinfo.R ^ 2 * dr ^ 2 - D ^ 2)) / (dr ^ 2)
+        yinterm = (-D * dx - np.abs(dy) * np.sqrt(caseinfo.R ^ 2 * dr ^ 2 - D ^ 2)) / (dr ^ 2)
+        ## Check which one of the intersection points lies in the segment
+        if (self.isinterior(xinterp, xBeamC)):
+            intX = xinterp
+            intY = yinterp
+        else:
+            intX = xinterm
+            intY = yinterm
+        ## Check that indeed you did everything right
+        assert(np.min(xBeamC, self.x) <= intX and intX <= np.max(xBeamC, self.x))
+        ## Use the point of intersection to calculate the depth of the voxel
+        self.depth = np.sqrt((intX - self.x)^2 + (intY - self.y)^2)
+
 
 ## Abstract class that implements a volume of interest with common location and radius. Parent of OAR and TARGET
 class VOI:
